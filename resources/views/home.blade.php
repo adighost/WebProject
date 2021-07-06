05b983c3c9f1d95638fe206d57d41e63da8bba72
adithya <adithyaghost130@gmail.com>
2021-07-06 10:25:04 +0530

    version 1.1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+      <div class="col-3">
+           <img src="/svm/freecode.jpg" style="max-width: 100%; height: auto;" alt="image"  >
+      </div>
+       <div class="col-9">
+          </div>
+       </div>
+    </div>
+@endsection
